122fca2ad30f12a3fcc9053a7acb387817a06443
Xuan <108101A1@gm.chihlee.edu.tw>
2022-12-11 23:29:07 +0800

    刪除多餘的import

--- src/com/gjun/studentsScore/util/SortUtil.java
+++ src/com/gjun/studentsScore/util/SortUtil.java
@@ -2,7 +2,6 @@ package com.gjun.studentsScore.util;
 
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.List;
 
 import com.gjun.studentsScore.model.StudentsReport;
 import com.gjun.studentsScore.model.StudentsScore;
